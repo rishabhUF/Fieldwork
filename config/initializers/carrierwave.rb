21b0ce9dae7c02aa7e922a6b84b7d951a70a1ffb
RISHABH SHARMA <rishabhsharma@Rishabhs-MacBook-Pro.local>
2017-11-21 17:32:10 -0500

    aws error 4

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: "AWS",
-    aws_access_key_id: "AKIAJLOHS7YZFYBLQ63Q",
-    aws_secret_access_key: "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"
+    aws_access_key_id: "AKIAJKKEA3OCSRTK6AXQ",
+    aws_secret_access_key: "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"
   }
   config.fog_directory = "fieldwire-ruby"
 end

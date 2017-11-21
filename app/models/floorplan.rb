49be3ff026b8a532da517dd3c88cd29c3d6d7909
RISHABH SHARMA <rishabhsharma@Rishabhs-MacBook-Pro.local>
2017-11-21 12:03:50 -0500

    carrierwave added with different version thumb nails

--- app/models/floorplan.rb
+++ app/models/floorplan.rb
@@ -1,3 +1,8 @@
 class Floorplan < ApplicationRecord
   belongs_to :project
+  validates_presence_of :name
+  validates :name, uniqueness: true
+
+  mount_uploader :screenshot, ImageUploader
+  mount_uploader :blueprint, ImageUploader
 end

434c460a7efefdeaf7224483a753347633c22924
RISHABH SHARMA <rishabhsharma@Rishabhs-MacBook-Pro.local>
2017-11-21 13:24:53 -0500

    final commit

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: "AWS",
-    aws_access_key_id: "AKIAI5EKVYSIP2JO27OA",
-    aws_secret_access_key: "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"
+    aws_access_key_id: "AKIAJ4Y26ZG73GWLTXTA",
+    aws_secret_access_key: "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"
   }
   config.fog_directory = "fieldwire-ror"
 end
